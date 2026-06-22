54391a7b3decde9661a9ed545d7cb39a0afb155c
Joko Hidayat <jojo@MacBook-Air-Joko.local>
2026-06-23 00:25:29 +0700

    Fix Vercel 500 error by configuring /tmp storage

--- api/index.php
+++ api/index.php
@@ -1,3 +1,20 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Setup Vercel /tmp directory
+$storagePath = '/tmp/storage';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath.'/framework/views', 0777, true);
+    mkdir($storagePath.'/framework/cache/data', 0777, true);
+    mkdir($storagePath.'/framework/sessions', 0777, true);
+    mkdir($storagePath.'/logs', 0777, true);
+}
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath($storagePath);
+
+$app->handleRequest(Request::capture());
